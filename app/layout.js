7b28a663d7e5108eeacf187b54cd283ada731120
Bani <152696529+itzbana@users.noreply.github.com>
2025-09-15 13:26:51 +0300

    added google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavbarGate from "@/components/NavbarGate"; // <-- add this
+import NavbarGate from "@/components/NavbarGate";
+import Script from 'next/script';
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -20,17 +21,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className="dark">
-            
-        <body className={`flex min-h-screen flex-col ${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <head>
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-KCDV4R337N"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-KCDV4R337N');
+                    `}
+                </Script>
+            </head>
 
-        {/* Shows everywhere EXCEPT paths that match HIDE_NAVBAR */}
-        <NavbarGate />
-        
-        <main className="max-w-2xl mx-auto lg:w-full flex-grow  px-6 sm:px-0 lg:px-0 py-8 ">
-        {children}
-        </main>
+            <body className={`flex min-h-screen flex-col ${geistSans.variable} ${geistMono.variable} antialiased`}>
+                {/* Shows everywhere EXCEPT paths that match HIDE_NAVBAR */}
+                <NavbarGate />
 
-        </body>
+                <main className="max-w-2xl mx-auto lg:w-full flex-grow  px-6 sm:px-0 lg:px-0 py-8 ">
+                    {children}
+                </main>
+            </body>
         </html>
     );
 }
