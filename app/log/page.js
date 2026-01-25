7734149d07b05d60621f4260611a1dd3913f8139
Bani <bbaltawalbeh23@cit.just.edu.jo>
2026-01-25 16:06:14 +0300

    new audit log page

---
+++ app/log/page.js
@@ -0,0 +1,47 @@
+
+export default function Log() {
+    return (
+        <div className="text-xs lg:text-[0.9rem]">
+            <ul>
+                <li>some assembly required <a href="https://github.com/hackclub/some-assembly-required">github repo</a></li>
+                <li>josdc verilog code, documentation</li>
+                <li>pplx at work <a href="https://r2cdn.perplexity.ai/pdf/pplx-at-work.pdf">doc</a></li>
+                <li>micro slides and <a href="https://notebooklm.google.com/notebook/410809a0-6f5d-41a6-90f8-435d9ab3650e">notebookllm</a></li>
+                <li>write your own <a href="https://www.jmeiners.com/lc3-vm/">virtual memory</a></li>
+                <li>mit course, <a href="https://computationstructures.org/">computation structures</a></li>
+                <li>read arm <a href="https://developer.arm.com/documentation/107829/0201/What-is-assembly-language-" >documentations</a></ li>
+                <li>circuitverse revise all <a href="https://learn.circuitverse.org/">digital topics</a></li>
+                <li>geeks <a href="https://www.geeksforgeeks.org/digital-logic/digital-electronics-logic-design-tutorials/">digital electronics</a></li>   
+                <li>solve all <a href="https://hdlbits.01xz.net/wiki/Problem_sets">hdlbits</a> verilog practices</li>
+                <li>work on the fl sheets project </li>
+                <li>Algorithms for <a href="https://en.algorithmica.org/hpc/">Modern Hardware</a></li>
+                <li>learn more c, <a href="https://www.learn-c.org/">this site</a></li>
+                <li>tutorial: digital to <a href="https://www.youtube.com/watch?v=FcFbFTbngrw&list=PLilenfQGj6CHLUHS3Y37OvtMeKdtv6Cxo&index=1">fpga 101, 1hour</a></li>
+                <li>architecture 1001, <a href="https://apps.p.ost2.fyi/learning/course/course-v1:OpenSecurityTraining2+Arch1001_x86-64_Asm+2021_v1/home">x86 assembly</a></li>
+            </ul>
+
+            <ul>
+                <li><s>yt, mostafah <a href="https://youtube.com/playlist?list=PLncIJXdNJMx-DQo9NNO0CsOpXa_Y-Y2tE&si=QuJvjIPnBQZrCoU4">digital compr course</a></s> <span className="text-gray-400 italic lg:text-[0.8rem]">1/25/2026</span></li>
+                <li>yt, anas, digital circuit <a href="https://www.youtube.com/playlist?list=PL-iIOnHwN7NXw01eBDR7wI8KzGK4mu8Sr">design Verilog</a></li>
+                <li>yt, anas, advanced digital <a href="https://youtube.com/playlist?list=PL-iIOnHwN7NUpkOWAQ9Fc7MMddai9vHvN&si=0_5gBogh4JkMZMs4">design SystemVerilog</a></li>
+                <li>yt, caleb, scaling apps <a href="https://youtube.com/playlist?list=PL_c9BZzLwBRLSs6x50D5WIH76VCUxJs9E&si=EbLhrr7bUnsVXnEp">system deisgn</a></li>
+                <li>yt, digikey, introduction to <a href="https://youtube.com/playlist?list=PLEBQazB0HUyT1WmMONxRZn9NmQ_9CIKhb&si=0VQ6w9vkgmmOXd35">fpga</a></li>
+                <li>yt, beneater, building an <a href="https://youtube.com/playlist?list=PLowKtXNTBypGqImE405J2565dvjafglHU&si=JoMbafruWt5vJ60B">8-bit breadboard computer</a></li>
+                <li>yt, beneater, digital electronics <a href="https://youtube.com/playlist?list=PLowKtXNTBypETld5oX1ZMI-LYoA2LWi8D&si=Txpkswl2Oc6WV7BB">tutorial</a></li>
+                <li>yt, beneater, crc hamming <a href="https://youtube.com/playlist?list=PLowKtXNTBypFWff2QjXCWuSfJDWcvE0Vm&si=8CcOea6r-pzUX7bX">error detection</a></li>
+                <li>yt, beneater, build a 65c02-based <a href="https://youtube.com/playlist?list=PLowKtXNTBypFbtuVMUVXNR0z1mu7dp7eH&si=N45sbvaNbb-xEJUv">computer from scratch</a></li>
+                <li>yt, beneater, tcb dns <a href="https://youtube.com/playlist?list=PLowKtXNTBypH19whXTVoG3oKSuOcw_XeW&si=J66M52IAPE0Qp3qs">networking tutorial</a></li>
+                <li>yt, anahr, computer and os <a href="https://youtube.com/playlist?list=PL8pYI62gCNsWZeum5ynEDd3PtD-C6K4s3&si=ZvcgX3bJ2gR8OfV5">history</a></li>
+                <li>yt, rj45, building a cpu from scratch <a href="https://youtube.com/playlist?list=PLilenfQGj6CEG6iZ4TQJ10PI7pCWsy1AO&si=IbO2HqohG1tpnjil"></a></li>
+                <li>yt, rj45, fpga stack machine cpu <a href="https://youtube.com/playlist?list=PLilenfQGj6CHO25aw7tUb6xoVvwgGQfA9&si=8zK_jRzkoUkaRtJG">from scratch</a></li>
+                <li>yt, intermation, uart i2c <a href="https://youtube.com/playlist?list=PLxfrSxK7P38WkBdcEFBLDTZ0Gx7l843xn&si=Biz5UTRgviBdmysb">serial protocols </a></li>
+                <li>yt, intermation, computer organization and <a href="https://youtube.com/playlist?list=PLxfrSxK7P38X7XfG4X8Y9cdOURvC7ObMF&si=jLKlNzDhjwtqUTOM">design fundamentals</a></li>
+                <li>yt, intermation, applied computer <a href="https://youtube.com/playlist?list=PLxfrSxK7P38XfD-_bOPiARp1PeTuba2Ja&si=slr9ZG9ckHyYldeG">architectures</a> </li>
+                <li>yt, intermation, embedded <a href="https://youtube.com/playlist?list=PLxfrSxK7P38U6zqC6rCex1-Ej6-0lTrsc&si=JpacvbTnDHlKoUG1">system design</a></li>
+            
+            </ul>
+
+        </div>
+
+    );
+}
